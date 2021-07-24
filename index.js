07f66ee2b1b4deff6584ac1de00764a63c20ebc8
luisdeseta <luis.deseta@gmail.com>
2021-07-24 00:52:33 -0300

    primera rutas de productos

--- index.js
+++ index.js
@@ -21,43 +21,31 @@ const sequelize = new Sequelize(DB, USER, PASS, {
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+//Importar Rutas
+const productos = require('./rutas/productos');
+
 //Rutas
 
-//const productos = require('./rutas/productos')
+app.use('/api/producto', productos);
+
 app.get('/', (req, res) => {
     res.json({
         estado: true,
         mensaje: 'funciona!'
     })
-});
-app.get('/productos', (req, res) => {
+}); 
+
+/* app.get('/productos', (req, res) => {
     sequelize.query("select * from productos", 
     { type: sequelize.QueryTypes.SELECT} )
     .then( (list) => {
 	    res.send(list);
-});
-});
+    });
+}); */
 
-// prueba creacion de modelo
-
-const User = sequelize.define("producto", {
-    name: DataTypes.TEXT,
-    favoriteColor: {
-      type: DataTypes.TEXT,
-      defaultValue: 'green'
-    },
-    age: DataTypes.INTEGER,
-    cash: DataTypes.INTEGER
-  });
-  
-
-app.post('/creartabla', async (req, res) => {
-    await User.sync();
-    const jane = await User.create({name: 'luis', favoriteColor: 'vede',age: 23, cash: 333})
-    //console.log(res.send(productos))
-    res.send('ok');
-});
+//
 
 app.listen(PORT, () => {
-    console.log(`Servidor escuchando en puesrto ${PORT}, YEAH!!!`);
+    console.log(`Servidor escuchando en puerto ${PORT}, YEAH!!!`);
 });
--- rutas/productos.js
+++ rutas/productos.js
@@ -1,6 +1,100 @@
 const router = require('express').Router();
+const { Sequelize, DataTypes, Model, QueryTypes } = require('sequelize');
+// const sequelize = require('../index.js') se puede requerir desde index
 
-app.get('/', (req, res) => {
+//conexion a la base de datos
+const USER = process.env.DB_USER;
+const PASS = process.env.DB_PASS;
+const DB = process.env.DB_NAME;
+const HOST = process.env.HOST;
+
+const sequelize = new Sequelize(DB, USER, PASS, {
+    host: HOST,
+    dialect: 'mysql',
+    loggin: false,
+    freezeTableName: true
+
+})
+
+
+//Creacion de modelo de Producto
+
+const Prod = sequelize.define("product", {
+    name: DataTypes.TEXT,
+    shortname: DataTypes.TEXT,
+    price: DataTypes.INTEGER,
+    description: DataTypes.TEXT,
     
+  },
+  {
+    timestamps: false
+  });
+  
+/**
+ *  @description Crea la tabla productos si no existe
+ * 
+ * */ 
+router.post('/createtable', async (req, res) => {
+    try {
+        await Prod.sync();
+        res.json({Mensaje: `Tabla ${Prod.tableName} creada con éxito`});
+        
+    } catch (error) {
+        res.json({error})        
+    }
 });
+
+/**
+ * @description Crear un producto en la tabla PRODUCTS
+ * 
+ * TODO AGREGAR MANEJO DE ERRORES
+ */
+
+router.post('/createproduct', async (req, res) => {
+    await sequelize.query(`
+    INSERT into products (name, shortname, price, description)
+    values (:_name, :_shortname, :_price, :_description)
+    `,{
+        type: sequelize.QueryTypes.INSERT,
+        replacements:{
+            _name: req.body.name,
+            _shortname: req.body.shortname,
+            _price: req.body.price,
+            _description: req.body.description
+        }
+
+    })
+    res.json({Status: 'Producto creado con éxito'})
+});
+
+/**
+ * @description actualiza un producto por id en la tabla productos
+ * TODO AGREGAR MANEJO DE ERRORES
+ */
+router.put('/createproduct', async (req, res) => {
+    await sequelize.query(`
+    UPDATE products set price = :_price, name = :_name
+    WHERE id = :_id
+    `,{
+        type: sequelize.QueryTypes.UPDATE,
+        replacements:{
+            _id: req.body.id,
+            _name: req.body.name,
+            _shortname: req.body.shortname,
+            _price: req.body.price,
+            _description: req.body.description
+        }
+
+    })
+    res.json({Status: "Update ok"})
+});
+
+
+router.get('/products', (req, res) => {
+    sequelize.query("select * from products", 
+    { type: sequelize.QueryTypes.SELECT} )
+    .then( (list) => {
+	    res.send(list);
+    });
+}); 
 module.exports = router
